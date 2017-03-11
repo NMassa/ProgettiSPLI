eeceb740cedac00be6e08ca0d994fcdfa2b3a126
Daniele <lvtdnl@gmail.com>
2017-03-11 18:34:22 +0100

    Aggiunto Client e Server TCP ed UDP

--- Progetto 1 - IpTables/main.py
+++ Progetto 1 - IpTables/main.py
@@ -1,12 +1,13 @@
 import threading
 import sys
-from pathlib import Path # if you haven't already done so
-root = str(Path(__file__).resolve().parents[1])
+#from pathl1ib import Path # if you haven't already done so          #Daniele: pathlib mi crea un errore. Senza funziona tutto lo stesso (o almeno sembra)
+#root = str(Path(__file__).resolve().parents[1])
 # Or
 #   from os.path import dirname, abspath
 #   root = dirname(dirname(abspath(__file__)))
-sys.path.append(root)
+#sys.path.append(root)
 from helpers.helpers import output
+from helpers.connection import Connection
 from helpers import config
 if __name__ == "__main__":
 
@@ -110,7 +111,15 @@ if __name__ == "__main__":
                                 port = int_option
                                 output(out_lck, "Selected port: %i" % port)
 
-                    #TODO: creare thread e socket stream o datagram a seconda della selezione ed inviare una serie di messaggi
+                        # TODO: creare thread
+                        c = Connection(host, protocol, port)
+                        try:
+                            c.connect()
+                            #threading._start_new_thread(c.connect,())
+                            output(out_lck, "Sending %s requests.." % protocol)
+                        except Exception:           #Daniele: non so che Exception da il multithreading
+                            output(out_lck, "Thread not initialized")
+
 
                 elif int_option == 2:
                     protocol = None
@@ -166,7 +175,17 @@ if __name__ == "__main__":
 
                     output(out_lck, "Selected port: %i" % port)
 
-                    # TODO: creare thread e socket in ascolto sulla porta specificata
+                    # TODO: creare thread
+
+                    c = Connection(None, protocol, port)
+                    try:
+                        #threading._start_new_thread(c.listen, ())
+                        c.listen()
+                        output(out_lck, "Listening on port %s.." % port)
+                    except Exception:                               # Daniele: non so che Exception da il multithreading
+                        output(out_lck, "Thread not initialized")
+
+
 
                 elif int_option == 3:
                     print("menu regole")
