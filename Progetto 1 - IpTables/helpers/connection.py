a61eea1f5568032cf11640865b4d11c68ac3d66f
massa <nkl.massa@hotmail.it>
2017-03-13 11:21:24 +0100

    bug fix

--- Progetto 1 - IpTables/helpers/connection.py
+++ Progetto 1 - IpTables/helpers/connection.py
@@ -1,6 +1,7 @@
 import socket
 from helpers import config
 from helpers import helpers
+import time
 import threading
 
 
@@ -37,19 +38,24 @@ class Connection:
 
             #Socket Datagram
             elif str(self.protocol) == "UDP":
-                self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                try:
-                    self.socket.connect((self.host, self.port))
-                    # qui devo fare un ciclo o un timer per mandare le richieste
-                    message = bytes("Sicurezza, Progettazione e Laboratorio Internet", encoding="utf8")
-                    for i in range(0, 40):
-                        self.socket.sendall(message)
+                for i in range(0, 30):
+                    self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    #self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    try:
+                        self.socket.connect((self.host, self.port))
+                        # qui devo fare un ciclo o un timer per mandare le richieste
+                        message = bytes("Sicurezza, Progettazione e Laboratorio Internet", encoding="utf8")
 
-                except socket.error as msg:
-                    helpers.output(self.out_lck, str(msg))
-                helpers.output(self.out_lck, "Done!")
-                self.socket.close()
+                        self.socket.sendall(message)
+                        self.socket.send('0'+ SIGEND)
+
+                        helpers.output(self.out_lck, "%d" % i)
+                        self.socket.close()
+
+                    except socket.error as msg:
+                        helpers.output(self.out_lck, str(msg))
+
+                    time.sleep(0.5)
             else:
                 #errori
                 exit()
