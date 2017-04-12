70e4f2b27bc298d2c6886ae74c458aaa22045f96
SteDev <zottiste@gmail.com>
2017-04-12 15:39:45 +0200

    decrypt frequency fix

--- Progetto 5 - Cifrario di Cesare/frequency.py
+++ Progetto 5 - Cifrario di Cesare/frequency.py
@@ -1,41 +1,41 @@
 import math
 from difflib import SequenceMatcher
+import operator
+import os
 
 
 class Frequency(object):
 
-    def __init__(self):
-        self.file_decrypt = object
+    def __init__(self, text):
+        self.file_decrypt = text
         self.dictionary_frequency = None
         self.dictionary_frequency = {}
         self.index = 0
-        self.file_input = open('frequency_input.txt', 'r')
-        self.list_file('u', ['books/(1) The Hunger Games.txt',
-                             'books/(2) Catching Fire.txt',
-                             'books/(3.1) Mockingjay.txt'])
+        self.file_input = open('helpers/frequency_input.txt', 'r')
+        #self.list_file = ['books/(1) The Hunger Games.txt', 'books/(2) Catching Fire.txt', 'books/(3.1) Mockingjay.txt']
+        self.list_file = [file for file in os.listdir('books/') if file.endswith('.txt')]
+
 
     def letter_frequency(self):
 
         while True:
             line = self.file_input.readline()
             if line == "":
-                print("finish")
                 break
             else:
                 letter, number = line.split()
                 self.dictionary_frequency[letter] = int(number)
         self.file_input.close()
 
-        file_output = open('frequency_output.txt', 'w')
+        file_output = open('helpers/frequency_output.txt', 'w')
         read_file = open(self.list_file[0], 'r')
 
         while True:
             letter2 = read_file.read(1)
             if letter2 == "":
-                print("finish")
                 break
             else:
-                self.index + 1
+                self.index = self.index + 1
                 if 96 < ord(letter2) < 123:
                     self.dictionary_frequency[letter2] = self.dictionary_frequency[letter2] + 1
                 elif 64 < ord(letter2) < 91:
@@ -43,12 +43,34 @@ class Frequency(object):
                     self.dictionary_frequency[chr(key)] = self.dictionary_frequency[chr(key)] + 1
 
         while True:
-            file_output.writelines('{} {}\n'.format(k, float(v / self.index)) for k, v in self.dictionary_frequency.items())
+
+            #trasformo il contatore di ogni lettera in una percentuale
+            for key, value in  self.dictionary_frequency.items():
+                self.dictionary_frequency[key] = self.dictionary_frequency[key]/self.index
+            #ordino il dizionario trasformandolo in una lista di coppie
+            dictionary_frequency_s = sorted(self.dictionary_frequency.items(), key=operator.itemgetter(1), reverse=True)
+            #file_output.writelines('{} {}\n'.format(k, float(v / self.index) * 100) for k, v in dictionary_frequency_s.items())
+            file_output.write('\n'.join('%s %s' % i for i in dictionary_frequency_s))
             file_output.write('\n')
             file_output.close()
             break
 
         read_file.close()
+        print("finish")
+
+    def crypt_file_frequency(self, file_crypt):
+        file = open(file_crypt, 'r')
+        dictionary_crypt = {}
+        while True:
+            line = file.readline()
+            if line == "":
+                break
+            else:
+                letter, number = line.split()
+                dictionary_crypt[letter] = int(number)
+        file.close()
+
+
 
     #restituisce percentuale
     def similar(a, b):
