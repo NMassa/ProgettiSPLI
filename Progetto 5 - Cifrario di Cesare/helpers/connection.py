2ca85d04421915390d9c16252d3494494c1567ee
JackJack92 <giacomovi156@gmail.com>
2017-04-12 14:47:29 +0200

    connection

--- Progetto 5 - Cifrario di Cesare/helpers/connection.py
+++ Progetto 5 - Cifrario di Cesare/helpers/connection.py
@@ -4,17 +4,58 @@ import copy
 import sys
 
 
-def caesar(plainText, shift):
-    cipherText = ""
-    for ch in plainText:
-        if ch.isalpha():
-            stayInAlphabet = ord(ch) + shift
-            if stayInAlphabet > ord('z'):
-                stayInAlphabet -= 26
-            finalLetter = chr(stayInAlphabet)
-            cipherText += finalLetter
-    # print "Your ciphertext is: ", cipherText
-    return cipherText
+
+
+def caesar(message, shift):
+    key = shift
+    translated = ''
+
+    for symbol in message:
+        if symbol.isalpha():
+            num = ord(symbol)
+            num += key
+
+            if symbol.isupper():
+                if num > ord('Z'):
+                    num -= 26
+                elif num < ord('A'):
+                    num += 26
+            elif symbol.islower():
+                if num > ord('z'):
+                    num -= 26
+                elif num < ord('a'):
+                    num += 26
+
+            translated += chr(num)
+        else:
+           translated += symbol
+    return translated
+
+
+def decaesar(message,shift):
+    key = -shift
+    translated = ''
+
+    for symbol in message:
+        if symbol.isalpha():
+            num = ord(symbol)
+            num += key
+
+            if symbol.isupper():
+                if num > ord('Z'):
+                    num -= 26
+                elif num < ord('A'):
+                    num += 26
+            elif symbol.islower():
+                if num > ord('z'):
+                    num -= 26
+                elif num < ord('a'):
+                    num += 26
+
+            translated += chr(num)
+        else:
+           translated += symbol
+    return translated
 
 
 def connect(Host, Port):
@@ -27,13 +68,13 @@ def connect(Host, Port):
     ListLib = []
     try:
         _socket.connect((host, port))
-        # qui devo fare un ciclo o un timer per mandare le richieste
+
         print("Insert Shift")
         shift = input()
 
         i = 1
-        # for file in os.listdir("~/PycharmProjects/SPLI/Progetto 5 - Cifrario di Cesare/books"):
-        for file in os.listdir("/Users/Giacomo/Documents"):
+        for file in os.listdir("books"):
+        #for file in os.listdir("/Users/Giacomo/Documents"):
             if file.endswith(".txt"):
                 print(i, file)
                 ListLib.append(str(file))
@@ -42,7 +83,7 @@ def connect(Host, Port):
         nfile = input()
         nf = int(nfile) - 1
         filename = copy.copy(ListLib[nf])
-        f = open(filename, 'rb')
+        f = open("books/"+filename, 'r')
         l = f.read(1024)
         cs = caesar(str(l), int(shift))
         tr = cs.encode()
@@ -68,34 +109,32 @@ def listen(Port):
         s.bind(('', port))  # inizializzazione della connessione
         s.listen(100)
         conn, addr = s.accept()
+        f = open('received_file.txt', 'wb')
         size = 1024
         data = conn.recv(size)
         while len(data) > 0:
             print ("Received: %s" % data)
+            f.write(data)
             data = conn.recv(size)
+        f.close()
+        print("Decifro file sapendo shift\n Inserisci shift \n")
+        shift = input()
+        f = open("received_file.txt", 'rb')
+        l = f.read(1024)
+        l = l.decode('utf8')
+        # print("leggo %s" %l)
+        dc = decaesar(str(l), int(shift))
+        tr = dc  # .encode()
+        while (l):
+            print('decipherText ', repr(tr))
+            l = f.read(1024)
+            l = l.decode('utf8')
+            dc = decaesar(str(l), int(shift))
+            tr = dc  # .encode()
+
+        f.close()
     except socket.error as msg:
         sys.exit(1)
         # helpers.output(self.out_lck, str(msg))
 
 
-  #  def menu():
-   #     print('---1--- Send message    --')
-    #    print('---2--- Receive message --')
-     #   print('---3--- List of file    --')
-      #  print('---0--- Exit            --')
-
-
-  #  if __name__ == '__main__':
-  #     menu()
-  #      print("choose an option:")
-  #      sc = input()
-  #      scelta = int(sc)
-  #      print("scelta", scelta)
-  #      if (scelta == 1):
-   #         connect()
-   #     if (scelta == 2):
-   #         listen()
-   #     if (scelta == 3):
-   #           for file in os.listdir("~/pycharmprojects/spli/progetto 5 - cifrario di cesare/books"):
-   #             if file.endswith(".txt"):
-    #                print(os.path.join("~/pycharmprojects/spli/progetto 5 - cifrario di cesare/books", file))
