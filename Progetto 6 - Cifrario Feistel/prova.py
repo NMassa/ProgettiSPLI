31b1d8e40e6dc8d9c84413cc07d1cfdee918f6f4
SteDev2 <zottiste@gmail.com>
2017-04-28 15:27:47 +0200

    List of file signatures

---
+++ Progetto 6 - Cifrario Feistel/prova.py
@@ -0,0 +1,28 @@
+import os
+
+if __name__ == "__main__":
+    stringa = ['89504e47da1aa', 'ffd8ffe00104a46', '424df640000', ' ', '89504e47da1aa', '47496383961181']
+            # [PNG,             JPG,                BMP,          RAW,  PNG, GIF]
+    try:
+        #file = open('/files/Periodic-Table.png', 'r')
+        # file = open('files/frequency_cypher.txt', 'r')
+        immagini = [file for file in os.listdir('/root/PycharmProjects/ProgettiSPLI/Progetto 6 - Cifrario Feistel/files/')]
+        file = open('/root/PycharmProjects/ProgettiSPLI/Progetto 6 - Cifrario Feistel/files/' + immagini[3], 'rb')
+
+    except OSError as e:
+        print("errore apertura file")
+        print(e)
+    else:
+        print("lettura file")
+        f_byte = file.read(8)
+        #compare_file(stringa , f_byte)
+        print(''.join([r'{:x}'.format(c) for c in f_byte]))
+        file.close()
+        pippo = ''.join([r'{:x}'.format(c) for c in f_byte])
+        for i in stringa:
+            if i == pippo:
+                print('ok')
+                break
+            else:
+                print('non trovato')
+
