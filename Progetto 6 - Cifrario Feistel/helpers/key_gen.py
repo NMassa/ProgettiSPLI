6db54248d999270a7f39231b6ecb5621e6ce2f14
JackJack92 <giacomovi156@gmail.com>
2017-05-02 16:13:05 +0200

    16 key generation 32

--- Progetto 6 - Cifrario Feistel/helpers/key_gen.py
+++ Progetto 6 - Cifrario Feistel/helpers/key_gen.py
@@ -1,8 +1,11 @@
 import random
-from helpers.utils import *
+#from helpers.utils import *
 
 def toBinary(n):
     return ''.join(str(1 & int(n) >> i) for i in range(8)[::-1])
+def toNum(b):
+    dec =int(b,2)
+    return dec
 
 def gen_keys(keyb):
     print("\t\t KEY8 \t")
@@ -22,6 +25,8 @@ def gen_keys(keyb):
             #print("Left: "+Left +"\tLeft1: "+Left1)
             #print("Right: "+Right +"\tRight1: "+Right1)
             print("KEYRET:"+keyret)
+            dec= toNum(keyret)
+            print("DEC:"+str(dec))
     #invert left and right
     Left2=Right
     Right2=Left
@@ -35,13 +40,55 @@ def gen_keys(keyb):
             #print("Left (Right): "+Right+"\tLeft1: "+Left2)
             #print("Right (Left): "+Left +"\tRight1: "+Right2)
             print("KEYRET:"+keyret)
+            dec = toNum(keyret)
+            print("DEC:" + str(dec))
     return keys
 
 #prendo chiave da 8 e restituisco tot chiavi da 32 tutte diverse (una per round-->8)
-def gen_key32(out_lck, keyb):
-    output(out_lck, "Key base: %s" % keyb)
-    output(out_lck, "Generating subkeys...")
-    #print("\t\t KEY32 \t")
+#def gen_key32(out_lck, keyb):
+def gen_8key32(keyb):
+
+    # output(out_lck, "Key base: %s" % keyb)
+    # output(out_lck, "Generating subkeys...")
+    print("\t\t 8 KEY32 \t")
+    i = 0
+    keys = []
+    Left = keyb[:len(keyb) // 2]
+    Right = keyb[len(keyb) // 2:]
+    Left1 = Left
+    Right1 = Right
+    # shift 1 Left and Right
+    for i in range(0, 4):
+        Left1 = Left1[(1 % len(Left1)):] + Left1[:(1 % len(Left1))]
+        Right1 = Right1[(1 % len(Right1)):] + Right1[:(1 % len(Right1))]
+
+        keyret = Left1 + Right1
+        key32 = keyret + keyret + keyret + keyret + keyret + keyret
+        keys.append(key32)
+        print("KEYRET:" + key32)
+        dec = toNum(key32)
+        print("DEC:" + str(dec))
+        # output(out_lck, "Subkey %s: %s" % (i, key32))
+
+    # invert left and right
+    Left2 = Right
+    Right2 = Left
+    for i in range(0, 4):
+        Left2 = Left2[(1 % len(Left2)):] + Left2[:(1 % len(Left2))]
+        Right2 = Right2[(1 % len(Right2)):] + Right2[:(1 % len(Right2))]
+
+        keyret = Left2 + Right2
+        key32 = keyret + keyret + keyret + keyret + keyret + keyret
+        keys.append(key32)
+        print("KEYRET:" + key32)
+        dec = toNum(key32)
+        print("DEC:" + str(dec))
+        # output(out_lck, "Subkey %s: %s" % (i + 4, key32))
+
+def gen_16key32(keyb):
+    #output(out_lck, "Key base: %s" % keyb)
+    #output(out_lck, "Generating subkeys...")
+    print("\t\t 16 KEY32 \t")
     i=0
     keys=[]
     Left = keyb[:len(keyb)//2]
@@ -56,8 +103,10 @@ def gen_key32(out_lck, keyb):
         keyret = Left1 + Right1
         key32 = keyret + keyret + keyret +keyret +keyret + keyret
         keys.append(key32)
-
-        output(out_lck, "Subkey %s: %s" % (i, key32))
+        print("KEYRET:" + key32)
+        dec = toNum(key32)
+        print("DEC:" + str(dec))
+        #output(out_lck, "Subkey %s: %s" % (i, key32))
 
     # invert left and right
     Left2 = Right
@@ -69,8 +118,38 @@ def gen_key32(out_lck, keyb):
         keyret = Left2 + Right2
         key32 = keyret + keyret + keyret + keyret + keyret + keyret
         keys.append(key32)
+        print("KEYRET:" + key32)
+        dec = toNum(key32)
+        print("DEC:" + str(dec))
+        #output(out_lck, "Subkey %s: %s" % (i + 4, key32))
+    #only left
+    Left2 = Left
+    Right2 = Left
+    for i in range(0, 4):
+        Left2 = Left2[(1 % len(Left2)):] + Left2[:(1 % len(Left2))]
+        Right2 = Right2[(1 % len(Right2)):] + Right2[:(1 % len(Right2))]
 
-        output(out_lck, "Subkey %s: %s" % (i + 4, key32))
+        keyret = Left2 + Right2
+        key32 = keyret + keyret + keyret + keyret + keyret + keyret
+        keys.append(key32)
+        print("KEYRET:" + key32)
+        dec = toNum(key32)
+        print("DEC:" + str(dec))
+        # output(out_lck, "Subkey %s: %s" % (i + 4, key32))
+    #only right
+    Left2 = Right
+    Right2 = Right
+    for i in range(0, 4):
+        Left2 = Left2[(1 % len(Left2)):] + Left2[:(1 % len(Left2))]
+        Right2 = Right2[(1 % len(Right2)):] + Right2[:(1 % len(Right2))]
+
+        keyret = Left2 + Right2
+        key32 = keyret + keyret + keyret + keyret + keyret + keyret
+        keys.append(key32)
+        print("KEYRET:" + key32)
+        dec = toNum(key32)
+        print("DEC:" + str(dec))
+        # output(out_lck, "Subkey %s: %s" % (i + 4, key32))
 
     return keys
 
@@ -81,4 +160,5 @@ if __name__ == "__main__":
     keyb= toBinary(int(key))
     print (keyb)
     gen_keys(str(keyb))
-    gen_key32(str(keyb))
+    gen_8key32(str(keyb))
+    gen_16key32(str(keyb))
